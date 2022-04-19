82b88ac992eb322c38cb4e8469109408f03b6fed
jhKim <nate851212@nate.com>
2022-04-19 22:49:32 +0900

    stream java

--- src/main/java/javaStudy/exceptionJava.java
+++ src/main/java/javaStudy/exceptionJava.java
@@ -4,6 +4,9 @@ package javaStudy;
 import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class exceptionJava {
     //Checked Exception Example
@@ -12,10 +15,10 @@ public class exceptionJava {
         boolean b = file.createNewFile();
     }
 
-    public static void slee(int millisecond){
-        try{
+    public static void slee(int millisecond) {
+        try {
             Thread.sleep(millisecond);
-        }catch(InterruptedException e){
+        } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
     }
@@ -41,22 +44,24 @@ public class exceptionJava {
     private void doSomething() throws retryException {
         int MAX_RETRY = 6;
         int maxRetry = MAX_RETRY;
-        while(maxRetry --> 0){
-            try{
+        while (maxRetry-- > 0) {
+            try {
                 // 예외가 발생할 가능성이 있는 시도
                 // 작업 성공 시 리턴
                 return;
-            }catch(Exception e){
+            } catch (Exception e) {
                 // 로그 출력, 정해진 시간 만큼 대기
-            }finally {
+            } finally {
                 // 리소스 반납 및 정리 작업
             }
         }
         throw new retryException(); // 최대 재시도 횟수를 넘기면 직접 예외 발생
     }
 
-    class retryException extends Throwable {};
-    class plusException extends Throwable {};
+    class retryException extends Throwable {
+    }
+
+    ;
 
     /*
     예외처리 회피
@@ -65,7 +70,10 @@ public class exceptionJava {
     하지만 무책임하게 던지는 것은 위험하다.
     호출한 쪽에서 다시 예외를 받아 처리하도록 하거나, 해당 메소드에서 이 예외를 던지는 것이 최선의 방법이라는 확신이 있을 때만 사용해야 한다.
     */
-    public void add() throws SQLException {}
+    public boolean add() throws SQLException {
+        return false;
+    }
+
     /*
     예외 전환
     예외를 잡아서(catch) 다른 예외를 던지는 것이다.
@@ -73,12 +81,15 @@ public class exceptionJava {
     어떤 예외인지 분명해야 처리가 수월해지기 때문이다.
     예를 들어 Checked Exception중 복구가 불가능한 예외가 잡혔다면, 이를 Unchecked Exception으로 전환하여 다른 계층에서 일일이 예외를 선언할 필요가 없도록 할 수도 있다.
     */
-    public void plus() {
-        try{
-            return;
-        }
-        catch(SQLException e){
-            throw DuplicateUserIdException();
+    public void slee2(int millisecond) {
+        try {
+            File file = new File("/test.txt");
+            boolean b = add();
+            List<String> listAge = new ArrayList<>();
+            listAge.stream().map(c->c.toLowerCase()).collect(Collectors.joining());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
